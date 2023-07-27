6530240c76fc1ac18b59a2edc54a470f6c53ec76
tsuchiya <tsuchiya@grabss.co.jp>
2023-07-27 16:04:34 +0900

    微調整

--- resources/js/Pages/Memo/Read.vue
+++ resources/js/Pages/Memo/Read.vue
@@ -32,52 +32,54 @@ function location() {
 </script>
 
 <template>
-    <div class='pt-6 sm:pt-0 bg-gray-100 flex flex-col sm:justify-center items-center'>
-        <div class='pt-6 w-full bg-gray-100 sm:max-w-md overflow-hidden sm:rounded-lg text-right'>
-            <div class='bg-white cursor-pointer overflow-hidden sm:rounded-lg inline-block text-center p-1' style='width: 140px;' @click="copyUrlToClipboard()">このページのURLをコピーする</div>
-            <div class='bg-white cursor-pointer overflow-hidden sm:rounded-lg inline-block text-center p-1 ml-2' style='width: 140px;' @click="switchDispModal()">URLをQRコードで表示する</div>
-        </div>
-    </div>
-
-    <GuestLayout style='justify-content: initial; min-height: auto;'>
-        <Head :title="`${props.memo.title}|MemoShare`"/>
-        <h1 class='font-bold text-xl'>{{props.memo.title}}</h1>
-        <p class='mt-4 text-sm content' style='white-space: pre-line;' v-html="autoLink(props.memo.content)"></p>
-        <p class='mt-4 text-sm text-right text-gray-600 '>最終更新：{{ dayjs(props.memo.updated_at).format('YYYY/MM/DD') }}</p>
-    </GuestLayout>
-
-    <div class='sm:pt-0 bg-gray-100 flex flex-col sm:justify-center items-center'>
-        <div class='py-6 w-full bg-gray-100 sm:max-w-md overflow-hidden sm:rounded-lg text-right'>
-            <Link
-                :href="route('memos.edit', props.memo.id)"
-                class="block cursor-pointer text-right underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-            >
-                メモを編集する
-            </Link>
-        </div>
-    </div>
-
-    <Modal :show="dispModal" @close="switchDispModal">
-        <div class="p-6">
-            <h2 class="text-lg font-medium text-gray-900">
-                QRコードを発行しました
-            </h2>
-
-            <p class="mt-1 text-sm text-gray-600">
-                読み取るとこのページにアクセスします。<br>
-                ページを共有したい人に読み取ってもらうか、QRコードの画像をLINEなどで送ってください。
-            </p>
-
-
-            <div class="text-center">
-                <vue-qrcode :value="location()" :options="{ width: 300 }" style='display: inline-block; margin: 20px 0;'></vue-qrcode>
-            </div>
-
-            <div class="text-center">
-                <SecondaryButton @click="switchDispModal" class='cursor-pointer'> 閉じる </SecondaryButton>
+    <div class='min-h-screen bg-gray-100'>
+        <div class='pt-6 sm:pt-0 flex flex-col sm:justify-center items-center'>
+            <div class='pt-6 w-full sm:max-w-md overflow-hidden sm:rounded-lg text-right'>
+                <div class='bg-white cursor-pointer overflow-hidden sm:rounded-lg inline-block text-center p-1' style='width: 140px;' @click="copyUrlToClipboard()">このページのURLをコピーする</div>
+                <div class='bg-white cursor-pointer overflow-hidden sm:rounded-lg inline-block text-center p-1 ml-2' style='width: 140px;' @click="switchDispModal()">URLをQRコードで表示する</div>
             </div>
         </div>
-    </Modal>
+
+        <GuestLayout style='justify-content: initial; min-height: auto;'>
+            <Head :title="`${props.memo.title}|MemoShare`"/>
+            <h1 class='font-bold text-xl'>{{props.memo.title}}</h1>
+            <p class='mt-4 text-sm content' style='white-space: pre-line;' v-html="autoLink(props.memo.content)"></p>
+            <p class='mt-4 text-sm text-right text-gray-600 '>最終更新：{{ dayjs(props.memo.updated_at).format('YYYY/MM/DD') }}</p>
+        </GuestLayout>
+
+        <div class='sm:pt-0 flex flex-col sm:justify-center items-center'>
+            <div class='py-6 w-full sm:max-w-md overflow-hidden sm:rounded-lg text-right'>
+                <Link
+                    :href="route('memos.edit', props.memo.id)"
+                    class="block cursor-pointer text-right underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                >
+                    メモを編集する
+                </Link>
+            </div>
+        </div>
+
+        <Modal :show="dispModal" @close="switchDispModal">
+            <div class="p-6">
+                <h2 class="text-lg font-medium text-gray-900">
+                    QRコードを発行しました
+                </h2>
+
+                <p class="mt-1 text-sm text-gray-600">
+                    読み取るとこのページにアクセスします。<br>
+                    ページを共有したい人に読み取ってもらうか、QRコードの画像をLINEなどで送ってください。
+                </p>
+
+
+                <div class="text-center">
+                    <vue-qrcode :value="location()" :options="{ width: 300 }" style='display: inline-block; margin: 20px 0;'></vue-qrcode>
+                </div>
+
+                <div class="text-center">
+                    <SecondaryButton @click="switchDispModal" class='cursor-pointer'> 閉じる </SecondaryButton>
+                </div>
+            </div>
+        </Modal>
+    </div>
 </template>
 
 <style>
