4ca8f972d18bfbb505026b23e656349cea18187b
tsuchiya <tsuchiya@grabss.co.jp>
2023-07-29 22:37:30 +0900

    メタタグを修正

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,6 +5,15 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <meta
+        property="og:description"
+        content="シンプルで使いやすいメモ共有サービス「MemoShare」。パソコンで書いたメモをスマホで確認、自分で書いたメモを他の人と共有。使い方はイロイロ。無料で始めてみましょう。"
+        />
+        <meta name="robots" content="all" />
+        <meta property="og:url" content="https://memo-with-laravel.fly.dev" />
+        <meta property="og:image" content="https://previews.dropbox.com/p/thumb/AB9Zy6_8cuB2fuoxyRtv1MxDKki-qIf8I8AOZ-fUPTbBOMtZny0LyT-9IiHSm7CZroJQlWB2AatgbouTMDBLuw4QCiskVXNi8fnyIVN8pDjcsLs4_XEUp61OM_rYvj5zvBqwy8OJ0fWPXeQrdcv4qmyqyDvSh4j1aw7InxAdEKy8wpiT2BLVzVZNMVTdmYug4o_6V8vP0sN-9Iji9n7FYb3H_ua1Cr49TMbEAUnEE4mjivd9rx_NC1Gdh2UZ_oaA2qcZYS3DRqHLQgVtFZ1NubB5lWr7TBexbNye4mpiPGNBCbF88sMO8OeEuBQnGJrlrNoCogHe90jLZCOkUkt0V3vL/p.png" />
+        <meta property="og:site_name" content="MemoShare" />
+        <meta property="og:locale" content="ja_JP" /> 
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
