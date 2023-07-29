98e7d50d6b9fcb42958f0b258b3814ad0be5c2d4
tsuchiya <tsuchiya@grabss.co.jp>
2023-07-29 12:31:57 +0900

    「メモを編集する」ボタンの表示条件を修正

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,74 +1,90 @@
 <script setup>
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import { Head,Link,usePage } from '@inertiajs/vue3';
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
+import { Head, Link, usePage } from "@inertiajs/vue3";
 </script>
 
 <template>
-    <AuthenticatedLayout>
-        <Head title="マイページ|MemoShare" />
-        <div v-if="$page.props.flash.message" class="bg-thin-gray px-28 py-4">
-            {{ $page.props.flash.message }}
+  <AuthenticatedLayout>
+    <Head>
+      <title>マイページ|MemoShare</title>
+      <meta name="description" content="Your page description" />
+    </Head>
+    <div
+      v-if="$page.props.flash.message"
+      class="bg-thin-gray px-28 py-4"
+    >{{ $page.props.flash.message }}</div>
+    <div class="memo-box-wrap p-12 flex gap-5 flex-wrap justify-center">
+      <div v-for="memo in memos" :key="memo.id">
+        <Link :href="route('memos.edit', memo.id)">
+          <div class="memo-box bg-secondry-red w-72 h-48 max-w-xs rounded-2xl p-4 m-2 relative">
+            <h2
+              class="text-lg font-bold"
+            >{{ memo.title ? (memo.title.length > 13 ? memo.title.substring(0, 13) + '...' : memo.title) : '' }}</h2>
+            <p>{{ memo.content ? (memo.content.length > 30 ? memo.content.substring(0, 30) + '...' : memo.content) : '' }}</p>
+            <p
+              class="text-right text-sm text-gray-100 bottom-4 right-4 absolute"
+            >{{ dayjs(memo.updated_at).format('YYYY/MM/DD') }} 更新</p>
+          </div>
+        </Link>
+      </div>
+      <Link :href="route('memos.create')">
+        <div
+          class="memo-box bg-secondry-red w-72 h-48 max-w-xs rounded-2xl p-4 m-2 relative"
+          style="  display: flex;justify-content: center;align-items: center;"
+        >
+          <img
+            :src="'/img/plus-icon.png'"
+            alt="register MemoShare"
+            class="add-memo"
+            style="max-width: 100px; max-height: 100px;"
+          />
         </div>
-        <div class="memo-box-wrap p-12 flex gap-5 flex-wrap justify-center">
-            <div v-for="memo in memos" :key="memo.id">
-                <Link :href="route('memos.edit', memo.id)">
-                    <div class='memo-box bg-secondry-red w-72 h-48 max-w-xs rounded-2xl p-4 m-2 relative'>
-                        <h2 class='text-lg font-bold'>{{ memo.title ? (memo.title.length > 13 ? memo.title.substring(0, 13) + '...' : memo.title) : '' }}</h2>
-                        <p>{{ memo.content ? (memo.content.length > 30 ? memo.content.substring(0, 30) + '...' : memo.content) : '' }}</p>
-                        <p class='text-right text-sm text-gray-100 bottom-4 right-4 absolute'>{{ dayjs(memo.updated_at).format('YYYY/MM/DD') }} 更新</p>
-                    </div>
-                </Link>
-            </div>
-            <Link :href="route('memos.create')">
-                <div class='memo-box bg-secondry-red w-72 h-48 max-w-xs rounded-2xl p-4 m-2 relative' style='  display: flex;justify-content: center;align-items: center;'>
-                    <img :src="'/img/plus-icon.png'" alt="register MemoShare" class='add-memo' style='max-width: 100px; max-height: 100px;'>
-                </div>
-            </Link>
-        </div>
-    </AuthenticatedLayout>
+      </Link>
+    </div>
+  </AuthenticatedLayout>
 </template>
 
 <script>
-import dayjs from "dayjs"
+import dayjs from "dayjs";
 
 export default {
   props: {
-    memos: Array,
-  },
-}
+    memos: Array
+  }
+};
 </script>
 
 <style>
-
 .memo-box {
-    min-height: 210px;
+  min-height: 210px;
 }
 
 .memo-box:hover {
-    cursor: pointer;
-    background: #CB191B;
-    color: white;
-    border-radius: 20px;
+  cursor: pointer;
+  background: #cb191b;
+  color: white;
+  border-radius: 20px;
 }
 
-@media (max-width: 1047px) { 
-   .memo-box {
-        max-width: 200px;
-    }
+@media (max-width: 1047px) {
+  .memo-box {
+    max-width: 200px;
+  }
 }
 
-@media (max-width: 547px) { 
-   .memo-box-wrap {
-        padding:1rem 0;
-        gap: 0;
-    }
+@media (max-width: 547px) {
+  .memo-box-wrap {
+    padding: 1rem 0;
+    gap: 0;
+  }
 
-    .memo-box {
-        max-width: 170px;
-    }
+  .memo-box {
+    max-width: 170px;
+  }
 }
 
 .add-memo:hover {
-    filter: invert(88%) sepia(61%) saturate(0%) hue-rotate(229deg) brightness(107%) contrast(101%);
+  filter: invert(88%) sepia(61%) saturate(0%) hue-rotate(229deg)
+    brightness(107%) contrast(101%);
 }
 </style>
--- resources/js/Pages/Memo/Read.vue
+++ resources/js/Pages/Memo/Read.vue
@@ -1,95 +1,124 @@
 <script setup>
-import GuestLayout from '@/Layouts/GuestLayout.vue';
-import Modal from '@/Components/Modal.vue';
-import { Head,Link } from '@inertiajs/vue3';
-import { ref } from 'vue';
+import GuestLayout from "@/Layouts/GuestLayout.vue";
+import Modal from "@/Components/Modal.vue";
+import { Head, Link } from "@inertiajs/vue3";
+import { ref } from "vue";
 import dayjs from "dayjs";
-import sanitizeHtml from 'sanitize-html';
-import VueQrcode from '@chenfengyuan/vue-qrcode';
+import sanitizeHtml from "sanitize-html";
+import VueQrcode from "@chenfengyuan/vue-qrcode";
 
 const dispModal = ref(false);
 
 const props = defineProps({
-    memo: Object,
-    is_owner: Boolean
+  memo: Object,
+  is_owner: Boolean
 });
 
 function autoLink(text) {
-    return sanitizeHtml(_.isString(text) ? text.replace(/(https?:\/\/[^\s]*)/g, "<a href='$1' target='_blank' rel='noopener noreferrer'>$1</a>") : '');
+  return sanitizeHtml(
+    _.isString(text)
+      ? text.replace(
+          /(https?:\/\/[^\s]*)/g,
+          "<a href='$1' target='_blank' rel='noopener noreferrer'>$1</a>"
+        )
+      : ""
+  );
 }
 
 function copyUrlToClipboard() {
-    return navigator.clipboard.writeText(window.location.href).then(function () { alert('URLをコピーしました。') })
+  return navigator.clipboard.writeText(window.location.href).then(function() {
+    alert("URLをコピーしました。");
+  });
 }
 
 function switchDispModal() {
-    dispModal.value = !dispModal.value;
+  dispModal.value = !dispModal.value;
 }
 
 function location() {
-    return window.location.href;
+  return window.location.href;
 }
 </script>
 
 <template>
-    <div class='min-h-screen bg-gray-100'>
-        <div class='pt-6 sm:pt-0 flex flex-col sm:justify-center items-center'>
-            <div class='pt-6 w-full sm:max-w-md overflow-hidden sm:rounded-lg text-right'>
-                <div class='bg-white cursor-pointer overflow-hidden sm:rounded-lg inline-block text-center p-1' style='width: 140px;' @click="copyUrlToClipboard()">このページのURLをコピーする</div>
-                <div class='bg-white cursor-pointer overflow-hidden sm:rounded-lg inline-block text-center p-1 ml-2' style='width: 140px;' @click="switchDispModal()">URLをQRコードで表示する</div>
-            </div>
-        </div>
-
-        <GuestLayout style='justify-content: initial; min-height: auto;'>
-            <Head :title="`${props.memo.title}|MemoShare`"/>
-            <h1 class='font-bold text-xl'>{{props.memo.title}}</h1>
-            <p class='mt-4 text-sm content' style='white-space: pre-line;' v-html="autoLink(props.memo.content)"></p>
-            <p class='mt-4 text-sm text-right text-gray-600 '>最終更新：{{ dayjs(props.memo.updated_at).format('YYYY/MM/DD') }}</p>
-        </GuestLayout>
-
-        <div class='sm:pt-0 flex flex-col sm:justify-center items-center'>
-            <div class='py-6 w-full sm:max-w-md overflow-hidden sm:rounded-lg text-right'>
-                <Link
-                    :href="route('memos.edit', props.memo.id)"
-                    class="block cursor-pointer text-right underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                >
-                    メモを編集する
-                </Link>
-            </div>
-        </div>
-
-        <Modal :show="dispModal" @close="switchDispModal">
-            <div class="p-6">
-                <h2 class="text-lg font-medium text-gray-900">
-                    QRコードを発行しました
-                </h2>
-
-                <p class="mt-1 text-sm text-gray-600">
-                    読み取るとこのページにアクセスします。<br>
-                    ページを共有したい人に読み取ってもらうか、QRコードの画像をLINEなどで送ってください。
-                </p>
-
-
-                <div class="text-center">
-                    <vue-qrcode :value="location()" :options="{ width: 300 }" style='display: inline-block; margin: 20px 0;'></vue-qrcode>
-                </div>
-
-                <div class="text-center">
-                    <SecondaryButton @click="switchDispModal" class='cursor-pointer'> 閉じる </SecondaryButton>
-                </div>
-            </div>
-        </Modal>
+  <div class="read-container min-h-screen bg-gray-100">
+    <div class="pt-6 sm:pt-0 flex flex-col sm:justify-center items-center">
+      <div class="pt-6 w-full sm:max-w-md overflow-hidden sm:rounded-lg text-right">
+        <div
+          class="bg-white cursor-pointer overflow-hidden sm:rounded-lg inline-block text-center p-1"
+          style="width: 140px;"
+          @click="copyUrlToClipboard()"
+        >このページのURLをコピーする</div>
+        <div
+          class="bg-white cursor-pointer overflow-hidden sm:rounded-lg inline-block text-center p-1 ml-2"
+          style="width: 140px;"
+          @click="switchDispModal()"
+        >URLをQRコードで表示する</div>
+      </div>
     </div>
+
+    <GuestLayout style="justify-content: initial; min-height: auto;">
+      <Head :title="`${props.memo.title}|MemoShare`" />
+      <h1 class="font-bold text-xl">{{props.memo.title}}</h1>
+      <p
+        class="mt-4 text-sm content"
+        style="white-space: pre-line;"
+        v-html="autoLink(props.memo.content)"
+      ></p>
+      <p
+        class="mt-4 text-sm text-right text-gray-600"
+      >最終更新：{{ dayjs(props.memo.updated_at).format('YYYY/MM/DD') }}</p>
+    </GuestLayout>
+
+    <div class="sm:pt-0 flex flex-col sm:justify-center items-center">
+      <div class="py-6 w-full sm:max-w-md overflow-hidden sm:rounded-lg text-right">
+        <Link
+          v-if="is_owner"
+          :href="route('memos.edit', props.memo.id)"
+          class="block cursor-pointer text-right underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+        >メモを編集する</Link>
+      </div>
+    </div>
+
+    <Modal :show="dispModal" @close="switchDispModal">
+      <div class="p-6">
+        <h2 class="text-lg font-medium text-gray-900">QRコードを発行しました</h2>
+
+        <p class="mt-1 text-sm text-gray-600">
+          読み取るとこのページにアクセスします。
+          <br />ページを共有したい人に読み取ってもらうか、QRコードの画像をLINEなどで送ってください。
+        </p>
+
+        <div class="text-center">
+          <vue-qrcode
+            :value="location()"
+            :options="{ width: 300 }"
+            style="display: inline-block; margin: 20px 0;"
+          ></vue-qrcode>
+        </div>
+
+        <div class="text-center">
+          <SecondaryButton @click="switchDispModal" class="cursor-pointer">閉じる</SecondaryButton>
+        </div>
+      </div>
+    </Modal>
+  </div>
 </template>
 
 <style>
-    @media (max-width:1215px) { 
-        .main-area {
-            padding: 15px;
-        }
-     }
-     
-     .content > a {
-        color: #0066cc;
-     }
+@media (max-width: 1215px) {
+  .main-area {
+    padding: 15px;
+  }
+}
+
+@media (max-width: 639px) {
+  .read-container {
+    padding: 10px;
+  }
+}
+
+.content > a {
+  color: #0066cc;
+}
 </style>
