9393398481a64dde476df4c1257e0523cb5b79ea
tsuchiya <tsuchiya@grabss.co.jp>
2023-07-31 19:25:35 +0900

    WIP：突tっページ

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -12,7 +12,7 @@ defineProps({
 </script>
 
 <template>
-  <GuestLayout style="justify-content: initial;">
+  <div>
     <div
       v-if="$page.props.flash.message"
       class="bg-thin-gray px-6 py-4"
@@ -32,42 +32,142 @@ defineProps({
       />
       <meta property="og:type" content="article" />
       <meta property="og:url" content="https://memo-with-laravel.fly.dev" />
-      <meta property="og:image" content="https://previews.dropbox.com/p/thumb/AB9Zy6_8cuB2fuoxyRtv1MxDKki-qIf8I8AOZ-fUPTbBOMtZny0LyT-9IiHSm7CZroJQlWB2AatgbouTMDBLuw4QCiskVXNi8fnyIVN8pDjcsLs4_XEUp61OM_rYvj5zvBqwy8OJ0fWPXeQrdcv4qmyqyDvSh4j1aw7InxAdEKy8wpiT2BLVzVZNMVTdmYug4o_6V8vP0sN-9Iji9n7FYb3H_ua1Cr49TMbEAUnEE4mjivd9rx_NC1Gdh2UZ_oaA2qcZYS3DRqHLQgVtFZ1NubB5lWr7TBexbNye4mpiPGNBCbF88sMO8OeEuBQnGJrlrNoCogHe90jLZCOkUkt0V3vL/p.png" />
+      <meta
+        property="og:image"
+        content="https://previews.dropbox.com/p/thumb/AB9Zy6_8cuB2fuoxyRtv1MxDKki-qIf8I8AOZ-fUPTbBOMtZny0LyT-9IiHSm7CZroJQlWB2AatgbouTMDBLuw4QCiskVXNi8fnyIVN8pDjcsLs4_XEUp61OM_rYvj5zvBqwy8OJ0fWPXeQrdcv4qmyqyDvSh4j1aw7InxAdEKy8wpiT2BLVzVZNMVTdmYug4o_6V8vP0sN-9Iji9n7FYb3H_ua1Cr49TMbEAUnEE4mjivd9rx_NC1Gdh2UZ_oaA2qcZYS3DRqHLQgVtFZ1NubB5lWr7TBexbNye4mpiPGNBCbF88sMO8OeEuBQnGJrlrNoCogHe90jLZCOkUkt0V3vL/p.png"
+      />
       <meta property="og:site_name" content="MemoShare" />
       <meta property="og:locale" content="ja_JP" />
     </Head>
-    <div style="display: flex;justify-content: center;align-items: center;">
-      <div>
-        <div class="text-center w-full rounded-b-2xl p-6 pb-0">
-          <h1 class="font-bold text-xl m-2">MemoShare</h1>
-          <p class="text-sm">パソコンで書いたメモをスマホで確認</p>
-          <p class="text-sm">自分で書いたメモを他の人と共有</p>
-          <img :src="'/img/top_img.png'" alt="start MemoShare" class="w-32 h-32 inline-block mt-4" />
-        </div>
-        <div>
-          <Link
-            :href="route('register')"
-            class="block text-right text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-          >
-            <PrimaryButton class="inline my-4">会員登録はこちら</PrimaryButton>
+
+    <div class="container">
+      <div class="box-top flex mx-10">
+        <div class="box-left w-5/12">
+          <h2 class="text-2xl">メモ共有サービス</h2>
+          <h1 class="text-5xl my-3 font-bold">MemoShare</h1>
+          <p>パソコンで書いたメモをスマホで確認、自分で書いたメモを他の人と共有</p>
+          <Link :href="route('register')">
+            <PrimaryButton class="inline my-6 primary-button">今すぐ始める</PrimaryButton>
           </Link>
-          <Link
-            :href="route('login')"
-            class="block text-right underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-          >ログインはこちら</Link>
+        </div>
+        <div class="box-right w-7/12">
+          <img :src="'/img/memo-ill.png'" class="top-img w-full" alt="start MemoShare" />
         </div>
       </div>
+      <div class="sec-use-case py-14 bg-secondry-red w-screen">
+        <h3 class="text-center text-3xl pb-16 font-bold use-case-lbl">こんな場面で</h3>
+        <div class="box-use-cases flex">
+          <div class="box-use-case text-center">
+            <h3 class="text-5xl">１</h3>
+            <h4 class="text-xl my-2 font-bold">買い物の時に</h4>
+            <div>レシピを眺めながら必要な材料をメモ。</div>
+            <div>メモを見ながらスーパーで材料を買えば</div>
+            <div>必要なものを漏れなく買えます。</div>
+          </div>
+          <div class="box-use-case text-center">
+            <h3 class="text-5xl">２</h3>
+            <h4 class="text-xl my-2 font-bold">旅行の計画を共有する時に</h4>
+            <div>SNSやメールで計画を共有すると</div>
+            <div>変更になる度に仲間へ連絡が必要です。</div>
+            <div>MemoShareなら1度URLを共有するだけで</div>
+            <div>修正の度に連絡する必要がなくなります。</div>
+          </div>
+          <div class="box-use-case text-center">
+            <h3 class="text-5xl">３</h3>
+            <h4 class="text-xl my-2 font-bold">外出先でメモを確認する時に</h4>
+            <div>オフィスではPCで必要な情報をメモ。</div>
+            <div>外出先ではスマホからメモをチェック。</div>
+            <div>PCとスマホのメモ共有の新たな手段。</div>
+          </div>
+        </div>
+      </div>
+
+      <div class="sec-use-case py-14 w-screen">
+        <h3 class="text-center text-3xl pb-16 font-bold use-case-lbl">使い方</h3>
+
+        <div class="box-how-to-uses mx-10">
+          <div class="box-how-to-use text-center flex">
+            <img :src="'/img/memo-ill.png'" style="width: 200px;" class alt="start MemoShare" />
+            <p>会員登録をします</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="sec-announce mx-10">
+        <div class="font-bold">免責事項</div>
+        <p>当サイトに掲載された内容によって生じた損害等の一切の責任を負いかねますので、その旨ご了承いただき、自己責任でのご利用をお願いいたします。</p>
+      </div>
     </div>
-  </GuestLayout>
+  </div>
 </template>
 
 <style>
-.bg-dots-darker {
-  background-image: url("data:image/svg+xml,%3Csvg width='30' height='30' viewBox='0 0 30 30' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M1.22676 0C1.91374 0 2.45351 0.539773 2.45351 1.22676C2.45351 1.91374 1.91374 2.45351 1.22676 2.45351C0.539773 2.45351 0 1.91374 0 1.22676C0 0.539773 0.539773 0 1.22676 0Z' fill='rgba(0,0,0,0.07)'/%3E%3C/svg%3E");
+.box-top {
+  width: calc(100vw - 80px);
 }
-@media (prefers-color-scheme: dark) {
-  .dark\:bg-dots-lighter {
-    background-image: url("data:image/svg+xml,%3Csvg width='30' height='30' viewBox='0 0 30 30' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M1.22676 0C1.91374 0 2.45351 0.539773 2.45351 1.22676C2.45351 1.91374 1.91374 2.45351 1.22676 2.45351C0.539773 2.45351 0 1.91374 0 1.22676C0 0.539773 0.539773 0 1.22676 0Z' fill='rgba(255,255,255,0.07)'/%3E%3C/svg%3E");
+
+.box-left {
+  margin: 140px auto;
+}
+
+.top-img {
+  height: 600px;
+  object-fit: cover;
+  object-position: top;
+  margin: 0 auto;
+}
+
+.box-use-cases {
+  gap: 40px;
+  justify-content: center;
+}
+
+.primary-button {
+  width: 250px;
+}
+
+.box-how-to-uses {
+}
+
+@media (max-width: 639px) {
+  .box-top {
+    display: inline-block;
+    width: 100%;
+  }
+
+  .box-left {
+    margin: initial;
+    margin-top: 40px;
+    width: calc(100% - 80px);
+  }
+
+  .primary-button {
+    width: 100%;
+  }
+
+  .use-case-lbl {
+    padding: 0;
+  }
+
+  .box-right {
+    width: calc(100% - 80px);
+  }
+
+  .top-img {
+    height: 150px;
+  }
+
+  .box-use-cases {
+    display: block;
+  }
+
+  .box-use-case {
+    margin: 40px 0;
+  }
+
+  .sec-use-case {
+    display: inline-block;
+    padding: 40px 0 20px 0;
   }
 }
 </style>
